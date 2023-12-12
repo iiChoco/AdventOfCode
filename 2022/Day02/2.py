bcd4fa79bf881f5fc0657d903af16ae4a201175e
Yunhan Wang <choco@Yunhans-MacBook-Pro.local>
2023-12-12 12:13:34 -0800

    idk why day 2 part 2 doesn't work for 2022

--- 2022/Day02/2.py
+++ 2022/Day02/2.py
@@ -1,28 +1,60 @@
-d = open("2.in").read().strip()
-lines = d.split("\n")
-score = 0
+import sys
 
-for i, line in enumerate(lines):
-    print(line)
-    if (line[0] == "A" and line[-1] == "X"):
-        score += 3
-    if (line[0] == "B" and line[-1] == "Y"):
-        score += 3
-    if (line[0] == "C" and line[-1] == "Z"):
-        score += 3
-    if (line[0] == "A" and line[-1] == "Y"):
-        score += 6
-    if (line[0] == "B" and line[-1] == "Z"):
-        score += 6
-    if (line[0] == "C" and line[-1] == "X"):
-        score += 6
-    if (line[-1] == "X"):
-        score += 1
-    if (line[-1] == "Y"):
-        score += 2
-    if (line[-1] == "Z"):
-        score += 3
-    print ("Iteration #" + str(i))
-    print(score)
-    print("="*80)
-print(score)
+d = open(sys.argv[1]).read().strip()
+lines = d.split("\n")
+
+
+def part1():
+    score = 0
+    for line in lines:
+        if line[0] == "A" and line[-1] == "X":
+            score += 3
+        if line[0] == "B" and line[-1] == "Y":
+            score += 3
+        if line[0] == "C" and line[-1] == "Z":
+            score += 3
+        if line[0] == "A" and line[-1] == "Y":
+            score += 6
+        if line[0] == "B" and line[-1] == "Z":
+            score += 6
+        if line[0] == "C" and line[-1] == "X":
+            score += 6
+        if line[-1] == "X":
+            score += 1
+        if line[-1] == "Y":
+            score += 2
+        if line[-1] == "Z":
+            score += 3
+    return score
+
+
+def part2():
+    count = 0
+    score = 0
+    for line in lines:
+        winMap = {"A": "Y", "B": "Z", "C": "X"}
+        tieMap = {"A": "X", "B": "Y", "C": "Z"}
+        loseMap = {"A": "Z", "B": "X", "C": "Y"}
+
+        opponent, me = line.split()
+        if me == "X":
+            me = loseMap[opponent]
+        if me == "Y":
+            me = tieMap[opponent]
+            score += 3
+        if me == "Z":
+            me = winMap[opponent]
+            score += 6
+
+        if me == "X":
+            score += 1
+        if me == "Y":
+            score += 2
+        if me == "Z":
+            score += 3
+        count += 1
+    print(count)
+    return score
+
+
+print(part2())
